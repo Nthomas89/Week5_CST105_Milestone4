6f658df953033143274aaf2a4bcf19dba8f18a74
Nthomas89 <34019043+Nthomas89@users.noreply.github.com>
2017-12-24 23:52:37 -0700

    Milestone 4

---
+++ NFL_Players.java
@@ -0,0 +1,106 @@
+/*
+ * Program: Milestone 4
+ * File: NFL_payers.java
+ * Summary: Modified NFL Player class
+ * Author: Nicholas Thomas
+ * Date: December 24, 2017
+ * This is my own work
+ */
+
+ abstract public class NFL_Players {
+	
+	//Declare variables
+			String name;
+			private String position;
+			private String team;
+			private int height;
+			private int weight;
+			private int age;
+			private int experience;
+			private String college;
+			
+			//Constructor without parameters
+			public NFL_Players() {
+				super();
+			}
+			
+			//Constructor that accepts all of the variables as parameters except BMI and rookieAge
+			public NFL_Players(String name, String position, String team, int height, int weight,
+					int age, int experience, String college) {
+				super();
+				this.name = name;
+				this.position = position;
+				this.team = team;
+				this.height = height;
+				this.weight = weight;
+				this.age = age;
+				this.experience = experience;
+				this.college = college;
+			
+			}
+			// getters and setters
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+			
+			public String getPosition() {
+				return position;
+			}
+			
+			public void setPosition(String position) {
+				this.position = position;
+			}
+			
+			public int getHeight() {
+				return height;
+			}
+			
+			public void setHeight(int height) {
+				this.height = height;
+			}
+			
+			public int getWeight() {
+				return weight;
+			}
+			
+			public void setWeight(int weight) {
+				this.weight = weight;
+			}
+			
+			public int getAge() {
+				return age;
+			}
+			
+			public void setAge(int age) {
+				this.age = age;
+			}
+
+			public int getExperience() {
+				return experience;
+			}
+
+			public void setExperience(int experience) {
+				this.experience = experience;
+			}
+
+			public String getCollege() {
+				return college;
+			}
+
+			public void setCollege(String college) {
+				this.college = college;
+			}
+
+			public String getTeam() {
+				return team;
+			}
+
+			public void setTeam(String team) {
+				this.team = team;
+			}
+
+}
